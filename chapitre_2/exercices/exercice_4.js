7ead71e8fb2d00ebec21022da0a5993a31f9321d
William-peel <lucas.duperier-catalaz@iut2.univ-grenoble-alpes.fr>
2019-10-15 16:04:14 +0200

    exo 4 vraiment fait

--- chapitre_2/exercices/exercice_4.js
+++ chapitre_2/exercices/exercice_4.js
@@ -1,1 +1,13 @@
 //Merci pour les exos
+let a = prompt("Saisi un nombre pd");
+function somme(n){
+    let somme =0;
+    n = parseInt(n);
+    while(n!=0){
+        somme = somme + n;
+        n--;
+    }
+    return somme;
+}
+
+alert(somme(a));
