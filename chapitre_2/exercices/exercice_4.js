503a0fb3ad8678ee5125890a7fd7e5b085e68874
William-peel <lucas.duperier-catalaz@iut2.univ-grenoble-alpes.fr>
2019-10-15 16:11:30 +0200

    exo 4 vraiment fait et corrigé pd

--- chapitre_2/exercices/exercice_4.js
+++ chapitre_2/exercices/exercice_4.js
@@ -1,12 +1,8 @@
 //Merci pour les exos
 let a = prompt("Saisi un nombre pd");
 function somme(n){
-    let somme =0;
     n = parseInt(n);
-    while(n!=0){
-        somme = somme + n;
-        n--;
-    }
+    let somme = n*(n+1)/2;
     return somme;
 }
 
