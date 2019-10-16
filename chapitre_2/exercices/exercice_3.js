26840641e75016c7efabb70c18e477a868d664d4
unknown <MIW@A-F004341.salsa.univ-amu.fr>
2019-10-16 15:03:47 +0200

    Ajout exo3

--- chapitre_2/exercices/exercice_3.js
+++ chapitre_2/exercices/exercice_3.js
@@ -1,8 +1,8 @@
 let n1 = parseFloat(prompt('Nombre 1'));
 let n2 = parseFloat(prompt('Nombre 2'));
 let n3 = parseFloat(prompt('Nombre 3'));
-alert('Nombres : '+n1+'|'+n2+'|'+n3+
+alert('Nombres : '+[n1,n2,n3].toString()+
     '\nSomme : '+(n1+n2+n3).toString()+
-    '\nMoyenne : '+((n1+n2+n3)/3).toString()+
-    '\nMax : '+Math.max(n1,n2,n3).toString()+' Min : '+Math.min(n1,n2,n3)+
-    '\nOrdre croissant : ');//TODO finir exo 3
+    '\nMoyenne : '+((n1+n2+n3)/3).toFixed(2)+
+    '\nMax : '+Math.max(n1,n2,n3).toString()+' | Min : '+Math.min(n1,n2,n3)+
+    '\nOrdre croissant : '+[n1,n2,n3].sort().toString());
