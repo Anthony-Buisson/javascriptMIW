e144d87e207b90b0364709dbc4dbdb06828cb80c
unknown <MIW@A-F004341.salsa.univ-amu.fr>
2019-10-16 16:56:17 +0200

    Explication exo4

--- chapitre_2/exercices/exercice_4.js
+++ chapitre_2/exercices/exercice_4.js
@@ -1,2 +1,10 @@
 let n = prompt('Saisir N : ');
 entierPositif(n)?alert('Somme des '+n+" premiers nombres : "+((parseInt(n)*(parseInt(n)+1))/2)):alert('N n\'est pas un entier positif');
+/*Explication ligne 2 :
+if(entierPositif(n)){
+    alert('Somme des '+n+' premiers nombres : ' + ((parseInt(n)*(parseInt(n)+1))/2));
+}
+else{
+    alert('N n\'est pas un entier positif');
+}
+*/
