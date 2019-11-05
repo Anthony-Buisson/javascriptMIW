14c78491f4ecbb1e02fbecc3c5b9a098cd2cdf64
unknown <MIW@A-F004341.salsa.univ-amu.fr>
2019-11-05 17:45:04 +0200

    Optimisation formulaire

--- tp/2/script.js
+++ tp/2/script.js
@@ -3,7 +3,7 @@ let prxProd = ['', 1000,80,150,20];
 
 function plus(n) {
     if(document.forms[0].product[n].value === '') return;
-    document.forms[0].quantity[n].value = document.forms[0].quantity[n].value*1 + 1;
+    document.forms[0].quantity[n].value = Number(document.forms[0].quantity[n].value) + 1;
 }
 function moins(n) {
     if(document.forms[0].quantity[n].value > 1 && document.forms[0].product[n].value !== '') document.forms[0].quantity[n].value -= 1;
@@ -18,7 +18,7 @@ function affMontant() {
     let quantities = document.forms[0].quantity;
     let prices = document.forms[0].price;
     for (let i = 0; i < quantities.length; i++) {
-        mtHT = mtHT*1 + (quantities[i].value*prices[i].value);
+        mtHT = Number(mtHT) + (quantities[i].value*prices[i].value);
     }
     document.getElementById('mtHT').value = mtHT.toFixed(2);
     document.getElementById('mtTVA').value = (mtHT*0.2).toFixed(2);
@@ -47,31 +47,32 @@ function affLigne(n) {
     document.forms[0].product[n].value !== '' ? document.forms[0].quantity[n].value = 1 : document.forms[0].quantity[n].value = '';
 }
 function creerLigneProduit(index) {
-    let select = '<select name="product" onchange="affLigne('+index+')">';
+    let select = `<select name="product" onchange="affLigne(${index})">`;
     for (let i = 0; i < libProd.length; i++) {
-        select+= '<option value="'+prxProd[i]+'">'+libProd[i]+'</option>';
+        select+= `<option value="${prxProd[i]}">${libProd[i]}</option>`;
     }
     return select+
-        '        <input name="price" type="number" value="" placeholder="Prix" readonly>' +
-        '        <input name="quantity" type="number" value="" placeholder="Quantité" readonly >' +
-        '        <button type="button" onclick="plus('+index+')">+</button>' +
-        '        <button type="button" onclick="moins('+index+')">-</button>' +
-        '        <button type="button" onclick="sup('+index+')">x</button>' +
-        '        <br>'
+        `<input name="price" type="number" value="" placeholder="Prix" readonly>
+         <input name="quantity" type="number" value="" placeholder="Quantité" readonly >
+         <button type="button" onclick="plus(${index})">+</button>
+         <button type="button" onclick="moins(${index})">-</button>
+         <button type="button" onclick="sup(${index})">x</button>
+         <br>`;
 }
 function afficheForm() {
-    let ch = '<form name="formulaireCommande">' +
-        '    <fieldset>' +
-        '        <legend>Identification</legend>' +
-        '        <label for="name">Nom : </label><input id="name" type="text" placeholder="Nom" required>' +
-        '        <label for="surname">Prénom : </label><input id="surname" type="text" placeholder="Prénom" required><br>' +
-        '        <label for="adress">Adresse : </label><input id="adress" type="text" required><br>' +
-        '        <label for="zipcode">Code Postal : </label><input name="zipcode" id="zipcode" type="text" pattern="\\d{5}" placeholder="00000" autocomplete="true" required>' +
-        '        <label for="city">Ville : </label><input id="city" type="text" placeholder="Paris" required><br>' +
-        '        <label for="phone">Téléphone : </label><input id="phone" type="tel" pattern="^(\\d{2}[ ]){4}\\d{2}$|^(\\d{2}[-]){4}\\d{2}$|^\\d{10}$|^(\\d{2}[.]){4}\\d{2}$" placeholder="01 23 45 67 89" required>' +
-        '        <label for="email">E-mail : </label><input id="email" type="email" pattern="^([\\w-]+(?:\\.[\\w-]+)*)@((?:[\\w-]+\\.)*\\w[\\w-]{0,66})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$" placeholder="Exemple: prenom@gmail.com" required>' +
-        '    </fieldset><fieldset onclick="affMontant()">' +
-        '        <legend>Sélection des produits</legend>';
+    let ch = `<form name="formulaireCommande">
+            <fieldset>
+                <legend>Identification</legend>
+                <label for="name">Nom : </label><input id="name" type="text" placeholder="Nom" required>
+                <label for="surname">Prénom : </label><input id="surname" type="text" placeholder="Prénom" required><br>
+                <label for="adress">Adresse : </label><input id="adress" type="text" required><br>
+                <label for="zipcode">Code Postal : </label><input name="zipcode" id="zipcode" type="text" pattern="\\d{5}" placeholder="00000" autocomplete="true" required>
+                <label for="city">Ville : </label><input id="city" type="text" placeholder="Paris" required><br>
+                <label for="phone">Téléphone : </label><input id="phone" type="tel" pattern="^(\\d{2}[ ]){4}\\d{2}$|^(\\d{2}[-]){4}\\d{2}$|^\\d{10}$|^(\\d{2}[.]){4}\\d{2}$" placeholder="01 23 45 67 89" required>
+                <label for="email">E-mail : </label><input id="email" type="email" pattern="^([\\w-]+(?:\\.[\\w-]+)*)@((?:[\\w-]+\\.)*\\w[\\w-]{0,66})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$" placeholder="Exemple: prenom@gmail.com" required>
+            </fieldset>
+            <fieldset onclick="affMontant()">
+                <legend>Sélection des produits</legend>`;
     ch+= creerLigneProduit(0);
     ch+= creerLigneProduit(1);
     ch+= creerLigneProduit(2);
