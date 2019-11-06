519801e6ce152da436f4fb59740b700915a02b80
unknown <MIW@A-F004341.salsa.univ-amu.fr>
2019-11-06 16:47:04 +0200

    Ajout exo1

---
+++ chapitre_4/exercices/exercice_1.js
@@ -0,0 +1,6 @@
+/*
+a)on crée un "raccourci" pour document.getelementbyid()
+b) ???
+c)on crée 2 fonctions globales
+d) On crée les fonctions merge, grep, each, trim dans $ pour les utiliser ensuite sous le format $.merge(t1,t2)
+ */
